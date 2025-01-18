d54712bcc38b89f87726a5d2c8a46d4d26eba2c4
Karan Krishna <160739817+KaranKrishna1@users.noreply.github.com>
2025-01-18 18:06:51 +0530

    Add files via upload

---
+++ partials-front/footer.php
@@ -0,0 +1,41 @@
+<!-- social Section Starts Here -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="https://www.instagram.com/thekarankrishna/?igsh=MTdmM3ZheDh5c3pjbw%3D%3D#" target="_blank"><img src="<?php echo SITEURL; ?>images/Instagram.png" alt="Instagram" style="height: 50px"></a>
+                </li>
+                <li>
+                    <a href="https://www.facebook.com/sameer.agarwal.73700136" target="_blank"><img src="<?php echo SITEURL; ?>images/Facebook.png" alt="Facebook" style="height: 50px"></a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/karan-krishna-8185552a1/"  target="_blank" ><img src="<?php echo SITEURL; ?>images/LinkedIn.png" alt="Linkedin" style="height: 50px"></a>
+                </li>
+                <li>
+                    <a href="https://github.com/KaranKrishna1" target="_blank"><img src="<?php echo SITEURL; ?>images/Github.png" alt="Github" style="height: 40px"></a>
+                </li>
+                <li>
+                    <a href="https://x.com/KaranKr11441061?t=alJoSpowoKpEikTcdX08uQ&s=08" target="_blank"><img src="<?php echo SITEURL; ?>images/X.png" alt="X" class="x" style="height: 35px"></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved | Designed By <a href="https://github.com/KaranKrishna1" style="color: rgb(92, 78, 197);">Karan Krishna</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
